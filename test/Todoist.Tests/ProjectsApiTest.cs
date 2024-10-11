6213eabd5670491d047c448d3f39df054865d8a9
tnakamura <griefworker@gmail.com>
2024-10-11 19:10:41 +0900

    Uninstall NSubstitute

--- test/Todoist.Tests/ProjectsApiTest.cs
+++ test/Todoist.Tests/ProjectsApiTest.cs
@@ -1,9 +1,7 @@
 ﻿using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using NSubstitute;
 using Xunit;
 using static Todoist.Constants.Endpoints;
 
